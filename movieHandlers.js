223655e2233de4975301a663d9e714da6f0421a7
Cédric PIERSON <cdric.pierson@gmail.com>
2022-11-02 08:26:36 +0100

    express dotenv exercice first commit

--- movieHandlers.js
+++ movieHandlers.js
@@ -25,23 +25,67 @@ const movies = [
   },
 ];
 
+const database = require("./database");
+
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    })
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movies])=>{
+      if(movies[0] != null){
+      res.json(movies[0]);
+      } else {res.status(404).send('Not Found ')}
+    })
+    .catch(err=>{
+      res.status(500).send("Error retrieving data from database");
+    })
 };
+
+const getUsers = (req,res) => {
+
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+      res.status(200);
+    })
+    .catch(err=>{
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    })
+}
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  database
+    .query("select * from users where id= ?", [id])
+    .then(([users]) => {
+      if(users[0] != null) {
+        res.status(200);
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found")
+      }
+    })
+}
 
 module.exports = {
   getMovies,
   getMovieById,
+  getUsers,
+  getUserById,
 };
